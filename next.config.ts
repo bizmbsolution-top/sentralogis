5a7f15bc78b12917dfe6743ef3d558fcbba1b1a5
bizmbsolution-top <biz.mbsolution@gmail.com>
2026-04-17 19:29:07 +0700

    fix: remove unsupported next.config options for vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // @ts-ignore
-  allowedDevOrigins: ['bubblingly-heapy-floria.ngrok-free.dev'],
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
 };
 
